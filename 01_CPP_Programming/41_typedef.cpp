70a40fa84986546db83bde83ca01a42250bd90ed
Abhay <ghongadeabhay60@gmail.com>
2024-12-18 19:18:29 +0530

    typedef

---
+++ 01_CPP_Programming/41_typedef.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// purpose of typedef is to make program more readable
+// by creating user defined data
+typedef int marks;
+
+int main(){
+    
+    marks m1, m2;
+
+    m1 = 50;
+    m2 = 90;
+
+}
