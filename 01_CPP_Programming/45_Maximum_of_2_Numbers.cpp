471fa67aa40d509538ae0dd7f53dd2b300853410
Abhay <ghongadeabhay60@gmail.com>
2024-12-19 19:56:55 +0530

    Maximum of 2 Numbers

---
+++ 01_CPP_Programming/45_Maximum_of_2_Numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void max(int x,int y)
+{
+    {
+    if(x>y)
+        cout<<x;
+    else
+        cout<<y;
+}
+    //write conditions here to print maximum of 2 numbers
+}
