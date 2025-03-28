600a2bee585168d9e85192304e0ac602750f8a35
Abhay <ghongadeabhay60@gmail.com>
2025-03-29 01:07:34 +0530

    Age of a persion coding exercise

---
+++ 02_CodingExercise/12_Age_of_a_persion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cin>>age;
+    //write conditional statement here
+    //output must be all in lower case
+    if (age >=12 && age <= 50)
+        cout<< "young";
+    else 
+    cout <<"not young";
+        
+}
